d86924a2047970de443e4bc2a0be1e1926ccaad9
IvnaBo <1361817133@qq.com>
2020-03-03 15:38:31 +0800

    linphoneSDK4.3

---
+++ LinphoneIvna.framework/Headers/LinphoneIvna.h
@@ -0,0 +1,25 @@
+//
+//  LinphoneIvna.h
+//  LinphoneIvna
+//
+//  Created by ivna.evecom on 2020/3/3.
+//  Copyright © 2020 ivna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+//! Project version number for LinphoneIvna.
+FOUNDATION_EXPORT double LinphoneIvnaVersionNumber;
+
+//! Project version string for LinphoneIvna.
+FOUNDATION_EXPORT const unsigned char LinphoneIvnaVersionString[];
+
+
+
+#import <LinphoneIvna/ESSipManager.h>
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinphoneIvna/PublicHeader.h>
+
+
